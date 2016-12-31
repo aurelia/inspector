b91ffb2339c5a22e190ec12488100ebc415ee79d
Rob Eisenberg <rob@bluespire.com>
2016-12-30 21:38:04 -0800

    feat(debug-host): expanding complex objects

--- src/backend/debug-host.ts
+++ src/backend/debug-host.ts
@@ -2,177 +2,209 @@ import {autoinject} from 'aurelia-dependency-injection';
 
 declare var $0: any;
 
-var processSelection = function () {
-  function setValue(debugInfo, value) {
-    let type;
-    let debugValue;
+var onSelectionChanged = function() {
+  let aureliaDebugger = (function() {
+    return window['aureliaDebugger'] || (window['aureliaDebugger'] = {
+      setValueOnDebugInfo(debugInfo, value) {
+        let type;
+        let debugValue;
 
-    if (value instanceof Node) {
-      debugInfo.type = 'node';
-      debugInfo.value = '[node]';
-    } else if (Array.isArray(value)) {
-      debugInfo.canExpand = true;
-      debugInfo.type = 'array';
-      debugInfo.value = '[array]';
-    } else {
-      debugInfo.type = typeof value;
-      debugInfo.value = value;
-    }
+        if (value instanceof Node) {
+          debugInfo.type = 'node';
+          debugInfo.value = '[node]';
+        } else if (Array.isArray(value)) {
+          debugInfo.canExpand = true;
+          debugInfo.type = 'array';
+          debugInfo.value = '[array]';
+        } else {
+          debugInfo.type = typeof value;
+          debugInfo.value = value;
+        }
 
-    if (debugInfo.type === 'object') {
-      debugInfo.canExpand = true;
+        if (debugInfo.type === 'object') {
+          debugInfo.canExpand = true;
+          debugInfo.debugId = this.getNextDebugId();
+          this.debugValueLookup[debugInfo.debugId] = value;
 
-      if (value.constructor) {
-        debugInfo.value = value.constructor.name;
-      } else {
-        debugInfo.value = '[object]';
-      }
-    }
+          if (value.constructor) {
+            debugInfo.value = value.constructor.name;
+          } else {
+            debugInfo.value = '[object]';
+          }
+        }
 
-    return debugInfo;
-  }
+        return debugInfo;
+      },
+      createControllerDebugInfo(controller) {
+        try {
+          let controllerDebugInfo: any = {
+            name: controller.behavior.elementName || controller.behavior.attributeName
+          };
+          
+          let viewModel = controller.viewModel;
 
-  function createControllerDebugInfo(controller) {
-    try {
-      let controllerDebugInfo: any = {
-        name: controller.behavior.elementName || controller.behavior.attributeName
-      };
-      
-      let viewModel = controller.viewModel;
+          controllerDebugInfo.bindables = controller.behavior.properties.map(x => {
+            return this.setValueOnDebugInfo({
+              name: x.name,
+              attribute: x.attribute,
+            }, viewModel[x.name]);
+          });
 
-      controllerDebugInfo.bindables = controller.behavior.properties.map(x => {
-        return setValue({
-          name: x.name,
-          attribute: x.attribute,
-        }, viewModel[x.name]);
-      });
+          controllerDebugInfo.properties = Object.keys(viewModel).filter(x => {
+            let found = controllerDebugInfo.bindables.find(x => x.name === x);
+            return !found && !x.startsWith('_');
+          }).map(x => {
+            return this.setValueOnDebugInfo({
+              name: x
+            }, viewModel[x]);
+          });
 
-      controllerDebugInfo.properties = Object.keys(viewModel).filter(x => {
-        let found = controllerDebugInfo.bindables.find(x => x.name === x);
-        return !found && !x.startsWith('_');
-      }).map(x => {
-        return setValue({
-          name: x
-        }, viewModel[x]);
-      });
+          return controllerDebugInfo;
+        } catch (e) {
+          return e.message;
+        }
+      },
+      _getRepeaterContext(node) {
+        var current = node.nextSibling;
 
-      return controllerDebugInfo;
-    } catch (e) {
-      return e.message;
-    }
-  }
+        while (current) {
+          if (current.nodeType === 8 && current.viewSlot) {
+            var children = current.viewSlot.children;
 
-  function _getRepeaterContext(node) {
-    var current = node.nextSibling;
+            for (var i = 0, ii = children.length; i < ii; ++i) {
+              var view = children[i];
+              var currentChild = view.firstChild
+              var lastChild = view.lastChild;
+              var nextChild;
 
-    while (current) {
-      if (current.nodeType === 8 && current.viewSlot) {
-        var children = current.viewSlot.children;
+              while (currentChild) {
+                nextChild = currentChild.nextSibling;
 
-        for (var i = 0, ii = children.length; i < ii; ++i) {
-          var view = children[i];
-          var currentChild = view.firstChild
-          var lastChild = view.lastChild;
-          var nextChild;
+                if (currentChild === node) {
+                  return view.bindingContext;
+                }
 
-          while (currentChild) {
-            nextChild = currentChild.nextSibling;
+                if (currentChild === lastChild) {
+                  break;
+                }
 
-            if (currentChild === node) {
-              return view.bindingContext;
+                currentChild = nextChild;
+              }
+            }
+          }
+
+          current = current.nextSibling;
+        }
+
+        return null;
+      },
+      _getBindingContext(node) {
+        if (!node) {
+          return null;
+        }
+
+        if (node.aurelia) {
+          return node.aurelia.root.viewModel;
+        } else if (node.au) {
+          var au = node.au;
+
+          if (au.controller) { //custom element
+            var controller = au.controller;
+            var tagName = node.tagName ? node.tagName.toLowerCase() : null;
+            var repeaterContext;
+
+            if (tagName === 'router-view') {
+              return controller.viewModel.view.controller.viewModel;
+            } else if (tagName === 'compose') {
+              return controller.viewModel.currentViewModel;
+            } else if (controller['with']) {
+              return controller['with'].viewModel.value;
+            } else if (repeaterContext = this._getRepeaterContext(node)) {
+              return repeaterContext;
+            } else {
+
+            }
+          }
+        }
+
+        return this._getBindingContext(node.parentNode);
+      },
+      getBindingContext(node) {
+        var repeaterContext;
+
+        if (repeaterContext = this._getRepeaterContext(node)) {
+          return repeaterContext;
+        }
+
+        return this._getBindingContext(node.parentNode);
+      },
+      convertObjectToDebugInfo(obj) {
+        return {
+          properties: Object.keys(obj).map(x => {
+            return this.setValueOnDebugInfo({
+              name: x
+            }, obj[x]);
+          })
+        };
+      },
+      selectNode() {
+        try {
+          var selectedNode = $0;
+          var debugInfo: any = {};
+
+          this.debugValueLookup = {};
+          this.nextDebugId = 0;
+
+          if (selectedNode.au) {
+            var au = selectedNode.au;
+
+            if (au.controller) {
+              debugInfo.customElement = this.createControllerDebugInfo(au.controller);
             }
 
-            if (currentChild === lastChild) {
-              break;
-            }
+            var tagName = selectedNode.tagName ? selectedNode.tagName.toLowerCase() : null;
+            var customAttributeNames = Object.keys(au).filter(function (key) {
+              return key !== 'controller' && key !== tagName;
+            });
 
-            currentChild = nextChild;
+            if (customAttributeNames.length) {
+              debugInfo.customAttributes = customAttributeNames.map(x => this.createControllerDebugInfo(au[x]));
+            }
+          }
+
+          debugInfo.bindingContext = this.convertObjectToDebugInfo(this.getBindingContext(selectedNode));
+
+          return debugInfo;
+        } catch(e) {
+          return this.createErrorObject(e);
+        }
+      },
+      getNextDebugId() {
+        return ++this.nextDebugId;
+      },
+      expandDebugValue(id) {
+        let value = this.debugValueLookup[id];
+        let debugInfo = this.convertObjectToDebugInfo(value);
+        return debugInfo;
+      },
+      createErrorObject(e) {
+        return {
+          bindingContext: {
+            properties: [
+              {
+                name: 'Error',
+                value: e.message,
+                type: 'string'
+              }
+            ]
           }
         }
       }
-
-      current = current.nextSibling;
-    }
-
-    return null;
-  }
-
-  function _getBindingContext(node) {
-    if (!node) {
-      return null;
-    }
-
-    if (node.aurelia) {
-      return node.aurelia.root.viewModel;
-    } else if (node.au) {
-      var au = node.au;
-
-      if (au.controller) { //custom element
-        var controller = au.controller;
-        var tagName = node.tagName ? node.tagName.toLowerCase() : null;
-        var repeaterContext;
-
-        if (tagName === 'router-view') {
-          return controller.viewModel.view.controller.viewModel;
-        } else if (tagName === 'compose') {
-          return controller.viewModel.currentViewModel;
-        } else if (controller['with']) {
-          return controller['with'].viewModel.value;
-        } else if (repeaterContext = _getRepeaterContext(node)) {
-          return repeaterContext;
-        } else {
-
-        }
-      }
-    }
-
-    return _getBindingContext(node.parentNode);
-  }
-
-  function getBindingContext(node) {
-    var repeaterContext;
-
-    if (repeaterContext = _getRepeaterContext(node)) {
-      return repeaterContext;
-    }
-
-    return _getBindingContext(node.parentNode);
-  }
-
-  function convertObjectToDebugInfo(obj) {
-    return {
-      properties: Object.keys(obj).map(x => {
-        return setValue({
-          name: x
-        }, obj[x]);
-      })
-    };
-  }
-
-  var selectedNode = $0;
-  var debugInfo: any = {};
-
-  if (selectedNode.au) {
-    var au = selectedNode.au;
-
-    if (au.controller) {
-      debugInfo.customElement = createControllerDebugInfo(au.controller);
-    }
-
-    var tagName = selectedNode.tagName ? selectedNode.tagName.toLowerCase() : null;
-    var customAttributeNames = Object.keys(au).filter(function (key) {
-      return key !== 'controller' && key !== tagName;
     });
+  })();
 
-    if (customAttributeNames.length) {
-      debugInfo.customAttributes = customAttributeNames.map(x => createControllerDebugInfo(au[x]));
-    }
-  }
-
-  debugInfo.bindingContext = convertObjectToDebugInfo(getBindingContext(selectedNode));
-
-  return debugInfo;
-};
+  return aureliaDebugger.selectNode();
+}
 
 export class SelectionChanged {
   constructor(public debugInfo) {}
@@ -184,8 +216,14 @@ export interface IHostConsumer {
 
 @autoinject
 export class DebugHost {
+  test = {
+    foo: 'message',
+    bar: {
+      something: 'sdfsdfsdfsdf'
+    }
+  };
   attach(consumer: IHostConsumer) {
-    let code = "(" + processSelection.toString() + ")()";
+    let code = "(" + onSelectionChanged.toString() + ")()";
     
     chrome.devtools.panels.elements.onSelectionChanged.addListener(() => {
       chrome.devtools.inspectedWindow.eval(code, debugObject => {
@@ -202,8 +240,13 @@ export class DebugHost {
     if (debugValue.canExpand) {
       debugValue.isExpanded = !debugValue.isExpanded;
 
-      if (debugValue.isExpanded) {
-        //TODO: eval in page
+      if (debugValue.isExpanded && !debugValue.expandedValue) {
+        let code = "aureliaDebugger.expandDebugValue(" + debugValue.debugId + ");";
+
+        chrome.devtools.inspectedWindow.eval(code, expandedValue => {
+          debugValue.expandedValue = expandedValue;
+          debugValue.isExpanded = true;
+        });
       }
     }
   }
